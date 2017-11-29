8271d98debee6c8dcb9b5ec32519a2a840caa829
Gerkin <nihilivin@gmail.com>
2017-11-29 04:16:46 +0100

    Option from args & config files

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const _ = require('lodash');
+const path = require('path');
+const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
-module.exports = args => {
-	_.defaults(args, {
+module.exports = config => {
+	const logLevel = _.findKey(winston.config.npm.levels, v => v === config.verbosity);
+	_.defaults(config, {
 		cwd: process.cwd(),
+		verbosity: 0,
+		packageFile: 'package.json',
+		configFile: '.dochow.json',
+		output: false,
+		singleFile: false,
+		templateDir: path.resolve(__dirname, 'resources'),
+		logger: winston.createLogger({
+			level: logLevel,
+			format: winston.format.combine(
+				winston.format.colorize({ all: true }),
+				winston.format.simple()
+			),
+			transports: [new winston.transports.Console()],
+		}),
 	});
+	config.logger.debug('Provided options: ', _.omit(config, 'logger'));
 
-	return documentProject( args, files, commander );
+	return require('./lib/documentProject')( config );
 }
